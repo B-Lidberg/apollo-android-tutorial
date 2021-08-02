2c733be9ba43408e352214dfd962044afca0d76d
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-08-01 20:44:33 -0400

    remove unused imports

--- app/src/main/java/com/example/rocketreserver/MainActivity.kt
+++ app/src/main/java/com/example/rocketreserver/MainActivity.kt
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.apollographql.apollo.coroutines.toFlow
+import com.example.rocketreserver.R
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,9 +27,9 @@ class MainActivity : AppCompatActivity() {
                 }
                 .collect {
                     val trips = it.data?.tripsBooked
-                    val text = when (trips) {
-                        null -> getString(R.string.subscriptionError)
-                        -1 -> getString(R.string.tripCancelled)
+                    val text = when {
+                        trips == null -> getString(R.string.subscriptionError)
+                        trips == -1 -> getString(R.string.tripCancelled)
                         else -> getString(R.string.tripBooked, trips)
                     }
                     Snackbar.make(
